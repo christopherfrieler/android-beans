9e90720784533f1b4a2cfbb380566dcaa3c2fbee
Christopher Frieler <christopher@frieler.rocks>
2017-08-27 15:55:44 +0200

    Implemented ForegroundActivityHolder to provide the current Activity.

---
+++ android-beans/src/main/java/rocks/frieler/android/beans/ForegroundActivityHolder.java
@@ -0,0 +1,54 @@
+package rocks.frieler.android.beans;
+
+import android.app.Activity;
+import android.app.Application;
+import android.os.Bundle;
+
+/**
+ * Implementation of the {@link android.app.Application.ActivityLifecycleCallbacks} that holds the {@link Activity}
+ * which is currently in the foreground (if any).
+ *
+ * @author Christopher Frieler
+ */
+public class ForegroundActivityHolder implements Application.ActivityLifecycleCallbacks {
+    private Activity currentActivity = null;
+
+    public Activity getCurrentActivity() {
+        return currentActivity;
+    }
+
+    @Override
+    public void onActivityCreated(Activity activity, Bundle bundle) {
+        this.currentActivity = activity;
+    }
+
+    @Override
+    public void onActivityStarted(Activity activity) {
+        this.currentActivity = activity;
+    }
+
+    @Override
+    public void onActivityResumed(Activity activity) {
+        this.currentActivity = activity;
+    }
+
+    @Override
+    public void onActivityPaused(Activity activity) {
+        if (this.currentActivity == activity) {
+            this.currentActivity = null;
+        }
+    }
+
+    @Override
+    public void onActivityStopped(Activity activity) {
+        if (this.currentActivity == activity) {
+            this.currentActivity = null;
+        }
+    }
+
+    @Override
+    public void onActivitySaveInstanceState(Activity activity, Bundle bundle) {}
+
+    @Override
+    public void onActivityDestroyed(Activity activity) {}
+}
