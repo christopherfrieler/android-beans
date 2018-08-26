cb653de05e6447cca6301367fd00d790913327b3
Christopher Frieler <christopher@frieler.rocks>
2018-08-26 18:42:45 +0200

    add support for optional BeanDependencys.

--- android-beans/src/test/java/rocks/frieler/android/beans/BeanConfigurationTest.java
+++ android-beans/src/test/java/rocks/frieler/android/beans/BeanConfigurationTest.java
@@ -8,6 +8,8 @@ import org.mockito.junit.MockitoJUnitRunner;
 import java.util.Arrays;
 import java.util.List;
 
+import java8.util.Optional;
+
 import static org.hamcrest.CoreMatchers.hasItem;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.collection.IsEmptyCollection.empty;
@@ -52,6 +54,19 @@ public class BeanConfigurationTest {
         };
 
         BeanDependency<BeanConfigurationTest> dependency = beanConfiguration.requireBean(BeanConfigurationTest.class);
+        List<BeanDependency<?>> dependencies = beanConfiguration.getDependencies();
+
+        assertThat(dependencies, hasItem(dependency));
+    }
+
+    @Test
+    public void testBeanConfigurationRequiresOptionalBeanByType() {
+        BeanConfiguration beanConfiguration = new BeanConfiguration() {
+            @Override
+            public void defineBeans(BeansCollector beansCollector) {}
+        };
+
+        BeanDependency<Optional<BeanConfigurationTest>> dependency = beanConfiguration.requireOptionalBean(BeanConfigurationTest.class);
         List<BeanDependency<?>> dependencies = beanConfiguration.getDependencies();
 
         assertThat(dependencies, hasItem(dependency));
