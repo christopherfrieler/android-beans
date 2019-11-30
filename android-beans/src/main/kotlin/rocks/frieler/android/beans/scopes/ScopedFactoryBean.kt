6ab3d2978fe3a3e1597014614cf73d3d21992ad2
Christopher Frieler <christopher@frieler.rocks>
2019-11-30 19:16:13 +0100

    convert ScopedFactoryBean to kotlin.

---
+++ android-beans/src/main/kotlin/rocks/frieler/android/beans/scopes/ScopedFactoryBean.kt
@@ -0,0 +1,32 @@
+package rocks.frieler.android.beans.scopes
+
+import android.app.Activity
+
+/**
+ * Interface for factory-beans, that produce the actual bean living in a limited scope, when needed.
+ *
+ * @param <T> the type of bean produced
+</T> */
+interface ScopedFactoryBean<T> {
+
+    /**
+     * Returns the name of the scope, where the bean produced by this [ScopedFactoryBean] lives in.
+     *
+     * @return the name of the scope
+     */
+    val scope: String
+
+    /**
+     * Returns the type of the bean produced by this [ScopedFactoryBean].
+     *
+     * @return the type of the bean
+     */
+    val beanType: Class<T>
+
+    /**
+     * Produces a new bean of type [T] corresponding to the given [Activity].
+     *
+     * @return a new bean of type [T] corresponding to the given [Activity]
+     */
+    fun produceBean(): T
+}
