c05b98fe09420cf37e30a27a5dc61e8cdd93bc36
Christopher Frieler <christopher@frieler.rocks>
2020-02-23 19:27:44 +0100

    update com.android.tools.build:gradle to 3.5.3.

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ buildscript {
         jcenter()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:3.5.2")
+        classpath("com.android.tools.build:gradle:3.5.3")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${Dependencies.kotlin_version}")
         classpath("org.jetbrains.dokka:dokka-android-gradle-plugin:0.9.18")
         classpath("com.jfrog.bintray.gradle:gradle-bintray-plugin:1.8.4")
