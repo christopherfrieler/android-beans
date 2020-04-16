a84727dfd852e9b8f03213e3bccfab0453fd2d3c
Christopher Frieler <christopher@frieler.rocks>
2020-04-16 20:40:47 +0200

    implement Beans facade with KClass for Kotlin.

--- android-beans/src/test/kotlin/rocks/frieler/android/beans/BeansTest.kt
+++ android-beans/src/test/kotlin/rocks/frieler/android/beans/BeansTest.kt
@@ -18,51 +18,82 @@ class BeansTest {
     }
 
     @Test
-    fun `lookUpBean() by name and type delegates to the BeansProvider`() {
+    fun `lookUpBean() by given type fetches bean from the BeansProvider`() {
+        val bean = Any()
+        whenever(beansProvider.lookUpBean(Any::class)).thenReturn(bean)
+
+        val retrievedBean = Beans.lookUpBean(type = Any::class)
+
+        assertThat(retrievedBean).isSameAs(bean)
+    }
+
+    @Test
+    fun `lookUpBean() by given type returns null without such bean`() {
+        whenever(beansProvider.lookUpBean(Any::class)).thenReturn(null)
+
+        val retrievedBean = Beans.lookUpBean(type = Any::class)
+
+        assertThat(retrievedBean).isNull()
+    }
+
+    @Test
+    fun `lookUpBean() by inferred type fetches bean from the BeansProvider`() {
+        val bean = Any()
+        whenever(beansProvider.lookUpBean(Any::class)).thenReturn(bean)
+
+        val retrievedBean: Any? = Beans.lookUpBean()
+
+        assertThat(retrievedBean).isSameAs(bean)
+    }
+
+    @Test
+    fun `lookUpBean() by given name and type fetches bean from the BeansProvider`() {
         val name = "bean"
         val bean = Any()
         whenever(beansProvider.lookUpBean(name, Any::class)).thenReturn(bean)
 
-        val retrievedBean = Beans.lookUpBean(name, Any::class.java)
+        val retrievedBean = Beans.lookUpBean(name, Any::class)
 
         assertThat(retrievedBean).isSameAs(bean)
     }
 
     @Test
-    fun `lookUpBean() by name and type returns null without such bean`() {
+    fun `lookUpBean() by given name and type returns null without such bean`() {
         val name = "bean"
         whenever(beansProvider.lookUpBean(name, Any::class)).thenReturn(null)
 
-        val retrievedBean = Beans.lookUpBean(name, Any::class.java)
+        val retrievedBean = Beans.lookUpBean(name, Any::class)
 
         assertThat(retrievedBean).isNull()
     }
 
     @Test
-    fun `lookUpBean() by type delegates to the BeansProvider`() {
+    fun `lookUpBean() by given name and  inferred type fetches bean from the BeansProvider`() {
+        val name = "bean"
         val bean = Any()
-        whenever(beansProvider.lookUpBean(Any::class)).thenReturn(bean)
+        whenever(beansProvider.lookUpBean(name, Any::class)).thenReturn(bean)
 
-        val retrievedBean = Beans.lookUpBean(Any::class.java)
+        val retrievedBean: Any? = Beans.lookUpBean(name)
 
         assertThat(retrievedBean).isSameAs(bean)
     }
 
     @Test
-    fun `lookUpBean() by type returns null without such bean`() {
-        whenever(beansProvider.lookUpBean(Any::class)).thenReturn(null)
-
-        val retrievedBean = Beans.lookUpBean(Any::class.java)
-
-        assertThat(retrievedBean).isNull()
-    }
-
-    @Test
-    fun `lookUpBeans() by type delegates to the BeansProvider`() {
+    fun `lookUpBeans() by given type delegates to the BeansProvider`() {
         val bean = Any()
         whenever(beansProvider.lookUpBeans(Any::class)).thenReturn(listOf(bean))
 
-        val retrievedBeans = Beans.lookUpBeans(Any::class.java)
+        val retrievedBeans = Beans.lookUpBeans(Any::class)
+
+        assertThat(retrievedBeans).containsExactly(bean)
+    }
+
+    @Test
+    fun `lookUpBeans() by inferred type delegates to the BeansProvider`() {
+        val bean = Any()
+        whenever(beansProvider.lookUpBeans(Any::class)).thenReturn(listOf(bean))
+
+        val retrievedBeans: List<Any> = Beans.lookUpBeans()
 
         assertThat(retrievedBeans).containsExactly(bean)
     }
